8503e93bc3ea22b88fc48491f4ea0b299cd94071
tannam76 <148177343+tannam76@users.noreply.github.com>
2026-05-20 20:23:32 +0700

    Update RoleRepository.java

--- src/main/java/com/example/demo/roles/repository/RoleRepository.java
+++ src/main/java/com/example/demo/roles/repository/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.roles.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.roles.model.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+    Optional<Role> findByName(String name);
+
 }
